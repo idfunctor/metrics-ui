d616e550015d8e65e6819c1644907971d731b8a7
idfunctor <helloworld@aviral.me>
2022-06-26 16:42:51 +0300

    cleanup

--- lib/metrics_ui/datasources_client.ex
+++ lib/metrics_ui/datasources_client.ex
@@ -8,12 +8,14 @@ defmodule MetricsUi.Datasources do
     case Github.get_repo(%{user_id: 'facebook', repo_id: 'react'}) do
       {:ok, data} -> data
         |> Map.get("stargazers_count")
-      {:error, _idc} -> "User not found."
+      # TODO: figure out better error handling
+      {:error, _idc} -> 0
     end
   end
 end
 
 defmodule MetricsUi.Structs.GithubRepo do
+  @enforce_keys [:stargazers_count]
   defstruct stargazers_count: 0
 end
 
@@ -23,24 +25,23 @@ defmodule MetricsUi.Datasources.Github do
   alias MetricsUi.Structs.GithubRepo
 
   plug Tesla.Middleware.BaseUrl, "https://api.github.com"
-  plug Tesla.Middleware.Headers, [{"user-agent", "Tesla"}]
 
   plug Tesla.Middleware.Headers, [
     {"authorization",
-     "token " <> Application.get_env(:metrics_ui, MetricsUi.Secrets)[:github_token]}
+     "token " <> Application.get_env(:metrics_ui, MetricsUi.Secrets)[:github_token]},
+     {"user-agent", "Tesla"}
   ]
 
   # plug Tesla.Middleware.JSON
-
   plug Tesla.Middleware.EncodeJson
   plug MetricsUi.Middleware.DecodeJsonAs
 
-  @spec get_repo(%{user_id: String.t(), repo_id: String.t()}) :: {:ok, MetricsUi.SGithubUser} | {:error, any}
+  @spec get_repo(%{user_id: String.t(), repo_id: String.t()}) :: {:ok, map()} | {:error, any}
   def get_repo(%{user_id: user_id, repo_id: repo_id}) do
-    get("/repos/#{user_id}/#{repo_id}")
+    get("/repos/#{user_id}/#{repo_id}", decode_as: %GithubRepo{stargazers_count: 0})
   end
 
   def get_repo_gazers(%{user_id: user_id, repo_id: repo_id}) do
-    get("/repos/#{user_id}/#{repo_id}/stargazers", decode_as: %GithubRepo{stargazers_count: 0})
+    get("/repos/#{user_id}/#{repo_id}/stargazers")
   end
 end
